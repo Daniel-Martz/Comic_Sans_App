b408cbc7429d1bb7e5116fe5153abaedb5ae86d1
Daniel-Martz <danieluniversidad006@gmail.com>
2026-02-20 17:16:40 +0100

    user algo mas

--- Main_Project/src/Client.java
+++ Main_Project/src/Client.java
@@ -3,13 +3,18 @@ import java.util.*;
 
 public class Client extends User{
 
-	private Set<Notification> notificaciones = new HashSet<>();
-	private Set<SecondHandProduct> ecHandProdcuts = new HashSet<>();
+	private Set<Notification> notifications = new HashSet<>();
+	private Set<SecondHandProduct> secondHandProducts = new HashSet<>();
 	
-	public Client(String username, String DNI, String password, Set<Notification> notification) {
+	public Client(String username, String DNI, String password, Set<Notification> notifications, Set<SecondHandProduct> secondHandProducts) {
 		super(username, DNI, password);
-		
+		this.notifications = (notifications != null) ? notifications : new HashSet<>();
+        this.secondHandProducts = (secondHandProducts != null) ? secondHandProducts : new HashSet<>();
 	}
 	
-
+	public void addProductToSecondHandProducts(SecondHandProduct product){
+		this.secondHandProducts.add(product);
+	}
+	
 }
+ 
