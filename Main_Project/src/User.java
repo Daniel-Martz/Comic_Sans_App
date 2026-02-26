59ebdb563b34cc5a7a1ba49e1c946aef365da703
MattTheCommiter <matteo.artu@gmail.com>
2026-02-26 21:52:07 +0100

    Creación de usuario

--- Main_Project/src/User.java
+++ Main_Project/src/User.java
@@ -4,12 +4,15 @@ public class User {
 	private String username;
 	private String DNI;
 	private String password;
+	private Notification n;
 	
 	
 	public User(String username, String DNI, String password) {
 		this.username = username;
 		this.DNI = DNI;
 		this.password = password;
+		this-n = new Notification();
+
 	}
 	
 	
