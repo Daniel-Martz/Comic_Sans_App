d6687c5a597d3a99d99a47227af3ed275ae08e1f
MattTheCommiter <matteo.artu@gmail.com>
2026-03-18 18:49:19 +0100

    Primeros métodos de usuario

--- Main_Project/src/usuario/Cartera.java
+++ Main_Project/src/usuario/Cartera.java
@@ -1,7 +1,17 @@
 package usuario;
 import java.util.*;
 
+import producto.ProductoSegundaMano;
+
 
 public class Cartera implements Contenedor<ProductoSegundaMano> {
 	private List<ProductoSegundaMano> productos = new ArrayList<>(); 
+	
+	public void añadirProducto(ProductoSegundaMano o) {
+		this.productos.add(o);
+	}
+	
+	public void eliminarProducto(ProductoSegundaMano o) {
+		this.productos.remove(o);
+	}
 }
