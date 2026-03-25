7b47cc8c262b8d3b300aa5a02e2b77f9a02dcaf8
Rodri0707 <rodru0707@gmail.com>
2026-03-25 18:05:06 +0100

    sistemapago

--- Main_Project/src/aplicacion/SistemaPago.java
+++ Main_Project/src/aplicacion/SistemaPago.java
@@ -1,21 +1,48 @@
-package aplicacion; 
+package aplicacion;
+
 import solicitud.*;
+import java.time.LocalDate;
 
 public class SistemaPago {
-	
+
 	private static SistemaPago instancia;
-	
+
 	private SistemaPago() {
 	}
-	
+
 	public static SistemaPago getInstancia() {
 		if (instancia == null) {
 			instancia = new SistemaPago();
 		}
 		return instancia;
 	}
-	
-	public Pago procesarPago(int importe, int numTarjeta, int cvv, int mesCaducidad, int añoCaducidad) {
-		return null; 
+
+	//faltan excepciones
+	public Pago procesarPago(double importe, String numTarjeta, String cvv, int mesCaducidad, int anioCaducidad) {
+
+		if (importe <= 0) {
+			return null;
+		}
+		if (numTarjeta == null || numTarjeta.length() != 16) {
+
+		}
+
+		if (cvv == null || cvv.length() != 3) {
+
+		}
+
+		LocalDate hoy = LocalDate.now();
+		int mesActual = hoy.getMonthValue();
+		int anioActual = hoy.getYear();
+
+		if (mesCaducidad < 1 || mesCaducidad > 12) {
+		}
+
+		if (anioCaducidad < anioActual || anioCaducidad == anioActual && mesCaducidad < mesActual) {
+
+		}
+		System.out.println("¡Pago de " + importe + "€ procesado con éxito!");
+		return null;
+		
 	}
 }
