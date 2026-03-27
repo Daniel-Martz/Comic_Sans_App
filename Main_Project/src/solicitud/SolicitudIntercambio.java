963f2887098d09547b871fdcdbc74b84a5be982a
Daniel-Martz <danieluniversidad006@gmail.com>
2026-03-27 12:19:20 +0100

    Validar ProductoSegundaMano y aprobar Intercambio

--- Main_Project/src/solicitud/SolicitudIntercambio.java
+++ Main_Project/src/solicitud/SolicitudIntercambio.java
@@ -7,6 +7,7 @@ public class SolicitudIntercambio extends Solicitud{
 	private CodigoIntercambio codigoOfertante;
 	private CodigoIntercambio codigoDestinatario;
 	private Oferta oferta;
+	private boolean aprobado = false;
 	
 	public SolicitudIntercambio(String codigoOfertante, String codigoDestinatario, String lugarIntercambio, Oferta oferta) {
 		this.codigoDestinatario = new CodigoIntercambio(codigoDestinatario);
@@ -15,12 +16,9 @@ public class SolicitudIntercambio extends Solicitud{
 		this.oferta = oferta;
 	}
 	
-	public boolean aprobarIntercambio(String codigoOfertante, String codigoDestinatario) {
+	public void aprobarIntercambio(String codigoOfertante, String codigoDestinatario) {
 		if(this.codigoOfertante.validarCodigo(codigoOfertante) && this.codigoDestinatario.validarCodigo(codigoDestinatario)) {
-			return true;
-		}
-		else {
-			return false;
+			aprobado = true;
 		}
 	}
 	
