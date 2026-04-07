83055b7bae43adf12c04ec92a7d5c4706522ecff
Rodri0707 <rodru0707@gmail.com>
2026-04-07 13:50:21 +0200

    aplicacion test

--- Main_Project/src/usuario/Empleado.java
+++ Main_Project/src/usuario/Empleado.java
@@ -127,5 +127,10 @@ public class Empleado extends UsuarioGestion {
 		
 		NotificacionPedido noti = new NotificacionPedido("El estado del pedido se ha actualizado a " + nuevoEstado, new DateTimeSimulado(), p);
 		p.getCliente().anadirNotificacion(noti);
-	} 
+	}
+
+	public List<NotificacionEmpleado> getNotificaciones() {
+		return notificaciones;
+	}
+	
 }
